3bea01b0c4cf10015a1d1e1c742addd5e824c4a3
MariaaPcsa <marialujivan@gmail.com>
2025-06-04 20:45:51 -0300

    html

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css'
-import './components/header/Header.css'
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
+import './index.css';
+import './components/header/Header.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import History from "./pages/history/History";
 
-
-
 function App() {
   return (
-   
-      <>
-        <BrowserRouter>
-       
-          <Header />
-
-          <Routes>
-           
-          <Route path="/" element={<Home/>} />
-          <Route path="/histoy" element={<History/>} />
-            {/* <Route path="/history" element={<History />} /> */}
-          </Routes>
-       
-        <Footer />
-            </BrowserRouter>
-      </>
-  
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
